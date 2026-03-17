8665b63e9048c5b7aff53515d61011a3e035a511
Faizan Ahmad <fiaznhmd.0624@gmail.com>
2026-03-18 03:50:21 +0530

    cllient

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,11 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+app.use(
+  cors({
+    origin: '*',
+  }),
+);
 app.use(cors());
 
 /**
@@ -112,4 +117,5 @@ app.get('/insights', async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
